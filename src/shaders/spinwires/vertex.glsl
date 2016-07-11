365010081b57d087682e26339107b7d2dc4113ff
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2016-07-11 18:24:25 +0200

    Add spinwires

---
+++ src/shaders/spinwires/vertex.glsl
@@ -0,0 +1,10 @@
+uniform sampler2D tDiffuse;
+
+varying vec2 vUv;
+varying vec3 vPosition;
+
+void main() {
+    vUv = uv;
+    vPosition = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
