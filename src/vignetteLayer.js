c2e1475de2465c1162943bb5828ecde4395591b4
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2016-07-14 21:36:28 +0200

    Update title scene ball to be a bit more alive

--- src/vignetteLayer.js
+++ src/vignetteLayer.js
@@ -4,6 +4,8 @@
 function vignetteLayer(layer) {
   this.config = layer.config;
   this.shaderPass = new THREE.ShaderPass(SHADERS.vignette);
+
+  this.random = Random(0x90);
 }
 
 vignetteLayer.prototype.getEffectComposerPass = function() {
@@ -17,8 +19,11 @@ vignetteLayer.prototype.end = function() {
 };
 
 vignetteLayer.prototype.update = function(frame, relativeFrame) {
-    if(BEAN < 288) {
-      this.shaderPass.uniforms.amount.value = smoothstep(10000, 0, relativeFrame / 442);
+    if(BEAN < 90) {
+      this.shaderPass.uniforms.amount.value = (
+          (smoothstep(10000, 0, relativeFrame / 442) * Math.sin(relativeFrame/2300) +
+          Math.sin(relativeFrame/12)*200)
+      );
     } else {
       this.shaderPass.uniforms.amount.value = 1;
     }
