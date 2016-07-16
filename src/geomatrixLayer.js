c16d6e23210d202132068780a9c68bddeb6fa570
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2016-07-16 12:51:36 +0200

    Tweak geometrix layer

--- src/geomatrixLayer.js
+++ src/geomatrixLayer.js
@@ -24,38 +24,12 @@ function geomatrixLayer(layer, demo) {
   pointLight.position.z = 130;
   this.scene.add(pointLight);
 
-  this.cone = new THREE.Mesh(new THREE.ConeGeometry(4, 60, 10),
-                             new THREE.ShaderMaterial(SHADERS.colorswappy)
-                            );
-  this.cone.position.x = 0;
-  this.cone.position.y = 0;
-
-  this.scene.add(this.cone);
-
-  this.numbers = [];
-  for(var i = 0; i < 12; i++) {
-    var num = new THREE.Mesh(new THREE.PlaneGeometry(40, 40),
-                             new THREE.MeshBasicMaterial({
-                               transparent: true,
-                               map: new THREE.Texture(this.generateNumber(''+(i+1)))
-                             })
-      );
-    num.position.z = 20;
-    num.position.y = Math.cos(i/2)*60;
-    num.position.x = Math.sin(i/2)*60;
-
-    this.numbers.push(num);
-    this.scene.add(num);
-  }
-
   this.plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 440),
                               new THREE.ShaderMaterial(SHADERS.lolshader));
   this.plane.rotation.x = 0;
   this.plane.rotation.z = -Math.PI / 4;
   this.plane.position.z = -10;
   this.scene.add(this.plane);
-  this.snareAnalysis = new audioAnalysisSanitizer('hihats.wav', 'spectral_energy', 0.05)
-
   this.renderPass = new THREE.RenderPass(this.scene, this.camera);
 }
 
@@ -73,39 +47,8 @@ geomatrixLayer.prototype.resize = function() {
 };
 
 geomatrixLayer.prototype.update = function(frame, relativeFrame) {
-  this.plane.material.uniforms.time.value = 100 + relativeFrame / 2;
-
-  this.cone.material.uniforms.time.value = frame;
-
-  var scale = Math.sin(relativeFrame/100);
-  this.cone.scale.set(scale, scale, scale);
-
-  this.cone.rotation.y = Math.sin(relativeFrame/1000)*200;
-
-  this.cone.lookAt(new THREE.Vector3(Math.cos(smoothstep(0, 12, relativeFrame/1000)/2)*60,
-                                     Math.sin(smoothstep(0, 12, relativeFrame/1000)/2)*60,
-                                     1)
-                  );
+  this.plane.material.uniforms.time.value = 200 + relativeFrame / 2;
 };
 
 geomatrixLayer.prototype.render = function(renderer, interpolation) {
-  for(var i = 0; i < this.numbers.length; i++) {
-    var num = this.numbers[i];
-    num.material.map.needsUpdate = true;
-  }
-};
-
-geomatrixLayer.prototype.generateNumber = function(num) {
-  var c = document.createElement('canvas');
-  var size = 100;
-  c.width = size;
-  c.height = size;
-
-  var ctx = c.getContext('2d');
-
-  ctx.fillStyle = 'white';
-  ctx.font = '40px Arial';
-  ctx.fillText(num, 38, 60);
-
-  return c;
 };
